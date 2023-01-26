3185479b41d68b660e3c842b6a7045492e651e15
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-26 18:31:45 +0300

    Добавлено задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Random rand = new Random();
+int randNum = rand.Next(100, 1000);
+System.Console.WriteLine($"Выпало число: {randNum}");
+int secondNum = (randNum%100)/10;
+System.Console.WriteLine($"Вторая цифра данного числа - {secondNum}");
+
+////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
