3d0c74505180788f7a66ddf629260d27b18bd42a
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-26 22:23:11 +0300

    Добавлено 3 задание

---
+++ task3/Program.cs
@@ -0,0 +1,46 @@
+﻿System.Console.Write("Введите номер дня недели ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day){
+   case 1:
+   { 
+      System.Console.WriteLine("Нет, брат, это будний день!");
+     break;
+   }
+   case 2:
+   { 
+      System.Console.WriteLine("Нет, брат, это будний день!");
+     break;
+   }
+   case 3:
+   { 
+      System.Console.WriteLine("Нет, брат, это будний день!");
+     break;
+   }
+   case 4:
+   { 
+      System.Console.WriteLine("Нет, брат, это будний день!");
+     break;
+   }
+   case 5:
+   { 
+      System.Console.WriteLine("Нет, брат, это будний день!");
+     break;
+   }
+   case 6:
+   { 
+      System.Console.WriteLine("Да, браток. Выходной!!!");
+     break;
+   }
+   case 7:
+   { 
+      System.Console.WriteLine("Да, браток. Выходной!!!");
+     break;
+   }
+   default:
+   {
+   System.Console.WriteLine("Дружок, ты по-моему перепутал , попробуй ещё!");
+   break;
+   }
+}
+
+
