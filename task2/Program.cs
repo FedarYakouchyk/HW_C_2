7496fff0df57efa97e941e6051d6492eb562a0d6
Fedor Yakouchyk <fedar77@outlook.com>
2023-01-26 20:32:56 +0300

    Добавлен 1 вариант второго задания

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Первый метод
+Console.Write("Введите любое 9-ти значное число: ");
+string num = Console.ReadLine();
+int size = num.Length;
+int N = Convert.ToInt32(num);
+System.Console.WriteLine($"Отлично, вы ввели {size} значное число");
+if (size<3)
+{
+    Console.WriteLine("Введённое вами число не является 3-х значным! Попробуйте снова!");
+}
+while (size>=3)
+{
+  if (size != 3)
+  {
+         size = size-1;
+    N= N/10;
+    
+  }  
+  else
+  {
+    size = size-1;
+    N= N%10;
+  }
+}
+System.Console.WriteLine($"Третья цифра вашего числа - {N}");
